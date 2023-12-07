b84bc3fdfc7e58a06ac064df53b993ff6b2450f7
ᴢєɴσꭙ <111524889+phoenix-monarch@users.noreply.github.com>
2023-12-07 19:29:41 +0530

    Update pm_filter.py

--- pm_filter.py
+++ pm_filter.py
@@ -1611,6 +1611,18 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('• ᴇᴀʀɴ ᴍᴏɴᴇʏ •', callback_data='source'),
             InlineKeyboardButton('• ʀᴜʟᴇꜱ •', callback_data='rule')
          ], [
+            InlineKeyboardButton('• ʏᴛ-ᴛʜᴜᴍʙ •', callback_data='ytthumb'),
+            InlineKeyboardButton('• ʏᴛ-ᴠɪᴅ •', callback_data='video'),
+            InlineKeyboardButton('• ʏᴛ-ᴛᴀɢꜱ •', callback_data='yttags')
+         ], [
+            InlineKeyboardButton('• ᴏᴡɴᴇʀ •', callback_data='mikey'),
+            InlineKeyboardButton('• ᴅᴏɴᴀᴛɪᴏɴ •', callback_data='owner_info'),
+            InlineKeyboardButton('• ɢɪᴛʜᴜʙ •', callback_data='github')
+         ], [
+            InlineKeyboardButton('• ᴀɴᴛᴇɪᴋᴜ •', url="https://t.me/AnteikuOfficial/161"),
+            InlineKeyboardButton('• ʀᴇᴘᴏʀᴛ •', callback_data='report'),
+            InlineKeyboardButton('• ɢᴇɴ-ᴘᴀss •', callback_data='gen_pass')
+         ], [
             InlineKeyboardButton('• ꜱᴇᴛᴛɪɴɢꜱ •', callback_data='settings'),
             InlineKeyboardButton('• ꜱᴛɪᴄᴋᴇʀ •', callback_data='sticker'), 
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ', callback_data='start')
