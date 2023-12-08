2e01f7b84020ff478873a590b0534eccdc07574b
ᴢєɴσꭙ <111524889+phoenix-monarch@users.noreply.github.com>
2023-12-08 22:16:41 +0530

    Update pm_filter.py

--- pm_filter.py
+++ pm_filter.py
@@ -2022,7 +2022,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InputMediaPhoto(random.choice(PICS))
         )
         await query.message.edit_text(
-            text=script.OPNAI,
+            text=script.OPNAI_TXT,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
