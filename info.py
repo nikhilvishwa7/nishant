57818f358cf33ec3e8562fb57ebb15e443997493
ᴢєɴσꭙ <111524889+phoenix-monarch@users.noreply.github.com>
2023-12-11 22:41:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ telethn = TelegramClient("Info", API_ID, API_HASH)
 CACHE_TIME = int(environ.get('CACHE_TIME', 99999))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/d4f60834f4b836230c4d0.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/d4f60834f4b836230c4d0.jpg https://graph.org/file/6c917ac3502cd37a5030b.jpg https://graph.org/file/ddef5154e8a7753a92b8e.jpg https://graph.org/file/bf634e576530322140504.jpg https://graph.org/file/1281fab3aea387b207716.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/41a08d01363c18ca909f1.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/d9221f67d9ba1e983b5de.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
