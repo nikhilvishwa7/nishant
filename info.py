cbcbd9a1e1e3c0be8b53388c8b35ea9ae7364c2b
ᴢєɴσꭙ <111524889+phoenix-monarch@users.noreply.github.com>
2023-12-18 10:44:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,6 +42,9 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
+# Command
+COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
+
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
