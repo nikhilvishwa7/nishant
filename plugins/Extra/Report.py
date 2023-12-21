a1c916332fc102611726963f631b29ee30213ba1
ᴢєɴσꭙ <111524889+phoenix-monarch@users.noreply.github.com>
2023-12-21 20:53:40 +0530

    Update Report.py

--- plugins/Extra/Report.py
+++ plugins/Extra/Report.py
@@ -6,26 +6,10 @@ from Script import script
 @Client.on_message((filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
 async def report_user(bot, message):
     if message.reply_to_message:
-        chat_id = message.chat.id
-        reporter = str(message.from_user.id)
-        mention = message.from_user.mention
-        success = True
-        report = f"𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})" + "\n"
-        report += f"𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {message.reply_to_message.link}"
-        # Using latest pyrogram's enums to filter out chat administrators
-        async for admin in bot.get_chat_members(chat_id=message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-            if not admin.user.is_bot: # Filtering bots and prevent sending message to bots | Message will be send only to user admins
-                try:
-                    reported_post = await message.reply_to_message.forward(admin.user.id)
-                    await reported_post.reply_text(
-                        text=report,
-                        chat_id=admin.user.id,
-                        disable_web_page_preview=True
-                    )
-                    success = True
-                except:
-                    pass
-            else: # Skipping Bots
-                pass
-        if success:
-            await message.reply_text(script.REPRT_MSG)
+        await message.reply_chat_action("typing")
+        k = await message.reply_text("**Processing...⏳**", quote=True)
+        await bot.send_message(PM, A.format(message.from_user.first_name, message.from_user.username, message.from_user.id, message.from_user.mention, message.from_user.id))
+        await message.reply_to_message.forward(chat_id=PM)
+        await k.edit_text("**Thanks for Reporting.** ❤️\n..\n**I have forwarded your message to my Owner. He will reply you When ever he will be free. ✌️**")
+    else:
+        await message.reply_text("**Please Send me a message** and **Then Reply that message with __/report__ , so that I can report that to my Owner**")
