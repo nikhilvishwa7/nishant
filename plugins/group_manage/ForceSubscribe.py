bf8f5a1f04bdf36542bdf1cc057d1e474375b971
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-01 16:50:21 +0530

    Update ForceSubscribe.py

--- plugins/group_manage/ForceSubscribe.py
+++ plugins/group_manage/ForceSubscribe.py
@@ -1,6 +1,6 @@
 import time
 import logging
-from Config import Config
+from info import environ
 from pyrogram import Client, filters
 from sql_helpers import forceSubscribe_sql as sql
 from pyrogram.types import ChatPermissions, InlineKeyboardMarkup, InlineKeyboardButton
