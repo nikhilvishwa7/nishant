f294495b0a83e9e95b729e08e1d336dfcfabae80
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-01 22:53:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -52,11 +52,6 @@ class temp(object):
     GETALL = {}
     SHORT = {}
     SETTINGS = {}
-    VERIFICATIONS = {}
-    FILES = {}
-    USERS_CANCEL = False
-    GROUPS_CANCEL = False
-    BOT = None
 
 async def is_subscribed(bot, query):
     try:
@@ -151,35 +146,40 @@ async def get_poster(query, bulk=False, id=False, file=None):
     }
 # https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
 
-async def broadcast_messages(user_id, message, pin):
+async def broadcast_messages(user_id, message):
     try:
-        m = await message.copy(chat_id=user_id)
-        if pin:
-            await m.pin(both_sides=True)
-        return "Success"
+        await message.copy(chat_id=user_id)
+        return True, "Success"
     except FloodWait as e:
-        await asyncio.sleep(e.value)
+        await asyncio.sleep(e.x)
         return await broadcast_messages(user_id, message)
-    except Exception as e:
+    except InputUserDeactivated:
         await db.delete_user(int(user_id))
-        return "Error"
-
-async def groups_broadcast_messages(chat_id, message, pin):
-    try:
-        k = await message.copy(chat_id=chat_id)
-        if pin:
-            try:
-                await k.pin()
-            except:
-                pass
-        return "Success"
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return await groups_broadcast_messages(chat_id, message)
+        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        return False, "Deleted"
+    except UserIsBlocked:
+        logging.info(f"{user_id} -Blocked the bot.")
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id} - PeerIdInvalid")
+        return False, "Error"
     except Exception as e:
-        await db.delete_chat(chat_id)
-        return "Error"
+        return False, "Error"
 
+async def broadcast_messages_group(chat_id, message):
+    try:
+        kd = await message.copy(chat_id=chat_id)
+        try:
+            await kd.pin()
+        except:
+            pass
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages_group(chat_id, message)
+    except Exception as e:
+        return False, "Error"
     
 async def search_gagala(text):
     usr_agent = {
@@ -299,14 +299,6 @@ def last_online(from_user):
         time += from_user.last_online_date.strftime("%a, %d %b %Y, %H:%M:%S")
     return time
 
-def get_readable_time(seconds):
-    periods = [('d', 86400), ('h', 3600), ('m', 60), ('s', 1)]
-    result = ''
-    for period_name, period_seconds in periods:
-        if seconds >= period_seconds:
-            period_value, seconds = divmod(seconds, period_seconds)
-            result += f'{int(period_value)}{period_name}'
-    return result
 
 def split_quotes(text: str) -> List:
     if not any(text.startswith(char) for char in START_CHAR):
