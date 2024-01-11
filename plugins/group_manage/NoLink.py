d1681830d2eb2b407b37cd0297af08f80410a746
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-11 17:38:49 +0530

    Update NoLink.py

--- plugins/group_manage/NoLink.py
+++ plugins/group_manage/NoLink.py
@@ -36,7 +36,7 @@ Bot = Client(
       
 
 
-@Client.on_message((filters.group) & filters.regex("@")  | filters.regex("t.me"))
+@Client.on_message((filters.group) filters.regex("t.me"))
 async def nolink(bot,message):
         
 	try:
