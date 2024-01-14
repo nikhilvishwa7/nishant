dac6199a32e0b40b61c1d334d1ce04041d52002f
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-14 22:05:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,6 +52,8 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001905367057')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
+pm = environ.get('PM')
+PM = int(pm) if pm and id_pattern.search(pm) else None
 
 # Command
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
