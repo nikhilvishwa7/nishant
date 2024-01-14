1d84fd3bd24d65efd45339547cb8dab73dac6d01
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-14 22:20:27 +0530

    Create config.py

---
+++ plugins/helper/config.py
@@ -0,0 +1,8 @@
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+ADMINS = int(environ.get("ADMINS", ""))          
+CAPTION = environ.get("CAPTION", "ʙʏ @team_netflix")
+
+# for thumbnail ( back end is MrMKN brain 😉)
+DOWNLOAD_LOCATION = "./DOWNLOADS"
