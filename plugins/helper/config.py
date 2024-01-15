e45674d57eb0ae246cb5934fe579146355cf54ec
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-15 17:23:36 +0530

    Delete plugins/helper/config.py

--- plugins/helper/config.py
+++
@@ -1,8 +0,0 @@
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMINS = int(environ.get("ADMINS", ""))          
-CAPTION = environ.get("CAPTION", "ʙʏ @team_netflix")
-
-# for thumbnail ( back end is MrMKN brain 😉)
-DOWNLOAD_LOCATION = "./DOWNLOADS"
