e2580202d84af20b4946f9ba8186416aaae24641
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-15 16:09:11 +0530

    Update dice.py

--- plugins/Extra/Games/dice.py
+++ plugins/Extra/Games/dice.py
@@ -3,7 +3,7 @@
 
 from pyrogram import Client, filters, enums
 from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
+from plugins.helper.cust_p_filters import f_onw_fliter
 
 # EMOJI CONSTANTS
 DICE_E_MOJI = "🎲"
