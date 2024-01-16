b49c9b00b25cceaa397a90b93a53adce73623a09
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-16 19:04:15 +0530

    Create infos.py

---
+++ plugins/helper/infos.py
@@ -0,0 +1,7 @@
+class Config(object):
+    OWNER_ID = 6497757690
+    ADMINS = [].append(OWNER_ID)
+    BOT_TOKEN = 
+    API_ID = 
+    API_HASH = 
+    mediaPattern = r"\b(https?://(?:(.*?)\.)?(?:instagram\.com|instagr\.am|t\.co|twitter\.com)(?:[^\s]*))\b"
