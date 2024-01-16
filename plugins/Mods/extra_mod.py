77f68040d059837c8c7df2067060015e0f051c84
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-16 11:22:59 +0530

    Update extra_mod.py

--- plugins/Mods/extra_mod.py
+++ plugins/Mods/extra_mod.py
@@ -1,5 +1,6 @@
 import requests
 from pyrogram import Client, filters, enums
+from info import BOT_USERNAME
 import time
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
