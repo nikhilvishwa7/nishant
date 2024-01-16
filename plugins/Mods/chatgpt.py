bdd3654056814ab2af84039d6964d361e3d379a8
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-16 18:19:22 +0530

    Create chatgpt.py

---
+++ plugins/Mods/chatgpt.py
@@ -0,0 +1,71 @@
+from pyrogram import Client, filters,enums,idle
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.enums import ChatAction, ParseMode
+
+from pyrogram.types import CallbackQuery
+from config import *
+import os,sys,re,requests
+import asyncio,time
+from random import choice
+from bardapi import Bard
+from datetime import datetime
+import logging
+
+
+@Client.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+async def chat(bot, message):
+    
+    try:
+        start_time = time.time()
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "Example:**\n\n`/chatgpt Where is TajMahal?`")
+        else:
+            a = message.text.split(' ', 1)[1]
+            response = requests.get(f'https://mukesh-api.vercel.app/chatgpt/{a}') 
+            x=response.json()["results"]
+            end_time = time.time()
+            telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ᴍs"
+            await message.reply_text(f"{message.from_user.first_name} ᴀꜱᴋᴇᴅ:\n\n {a} \n\n {BOT_NAME} ᴀɴꜱᴡᴇʀᴇᴅ:-\n\n {x}\n\n✨ᴛɪᴍᴇ ᴛᴀᴋᴇɴ  {telegram_ping} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{BOT_USERNAME} ", parse_mode=ParseMode.MARKDOWN,reply_markup=InlineKeyboardMarkup(X))     
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
+
+
+openai.api_key = OPENAI_API
+@Client.on_message(filters.command(["image","photo","img","generate"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"] ))
+async def chat(bot, message):
+    try:
+        start_time = time.time()
+        await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "**Example:**\n\n`/generate a white siamese cat`")
+        else:
+            a = message.text.split(' ', 1)[1]
+            response= openai.Image.create(prompt=a ,n=1,size="1024x1024")
+            image_url = response['data'][0]['url']
+            end_time = time.time()
+            telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ᴍs"
+            await message.reply_photo(image_url,caption=f"✨ᴛɪᴍᴇ ᴛᴀᴋᴇɴ {telegram_ping} ",parse_mode=ParseMode.DISABLED,reply_markup=InlineKeyboardMarkup(X)) 
+    except Exception as e:
+            await message.reply_text(f"**ᴇʀʀᴏʀ: **  ` {e} `")
+openai.api_key = OPENAI_API
+@Client.on_message(filters.command(["text","audiototext","lyrics"],  prefixes=["","+", ".", "/", "-", "?", "$","#","&"]))
+async def chat(bot, message):
+    
+    try:
+        start_time = time.time()
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if message.reply_to_message and message.reply_to_message.media:
+            
+            m = await message.reply_to_message.download(file_name="mukesh.mp3")
+            audio_file = open(m, "rb")
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+            x=transcript["text"]
+            end_time = time.time()
+            telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ᴍs"
+            await message.reply_text(f"`{x}` \n ✨ᴛɪᴍᴇ ᴛᴀᴋᴇɴ {telegram_ping}")     
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ: **  ` {e} `")
