61d310b4f82e0b7bd6b8d67b5377414e520c643d
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-16 21:03:13 +0530

    Delete plugins/helper/infos.py

--- plugins/helper/infos.py
+++
@@ -1,4 +0,0 @@
-class Config(object):
-    OWNER_ID = 6497757690
-    ADMINS = [].append(OWNER_ID)
-    mediaPattern = r"\b(https?://(?:(.*?)\.)?(?:instagram\.com|instagr\.am|t\.co|twitter\.com)(?:[^\s]*))\b"
