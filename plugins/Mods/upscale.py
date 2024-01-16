07ce7aaae02efb943aab750bc9dde4d5adbc67ea
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-16 09:56:59 +0530

    Update upscale.py

--- plugins/Mods/upscale.py
+++ plugins/Mods/upscale.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters, types as t
-from plugins.Mods import getFile, UpscaleImages
+from plugins.Mods import getFile,UpscaleImages
 import os
 
 @Client.on_message(filters.command(["upscale"]))
