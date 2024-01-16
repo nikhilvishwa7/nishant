da0db500bafc7157f911c80eccf8e2bc3051ecb0
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-16 11:15:15 +0530

    Update extra_mod.py

--- plugins/Mods/extra_mod.py
+++ plugins/Mods/extra_mod.py
@@ -1,5 +1,5 @@
 import requests
-from .. import pbot as Mukesh, BOT_USERNAME
+from pyrogram import Client, BOT_USERNAME
 import time
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
