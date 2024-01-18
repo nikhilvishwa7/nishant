18fc6e31ebc620f78607bf09a94649849e343d56
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-18 15:09:12 +0530

    Update Report.py

--- plugins/Extra/Report.py
+++ plugins/Extra/Report.py
@@ -1,3 +1,5 @@
+# Updated by @Joelkb | For Paid Edits Contact @creatorbeatz on Telegram
+
 import asyncio
 import os
 from pyrogram import filters, enums, Client 
@@ -6,10 +8,25 @@ from Script import script
 @Client.on_message((filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
 async def report_user(bot, message):
     if message.reply_to_message:
-        await message.reply_chat_action("typing")
-        k = await message.reply_text("**Processing...⏳**", quote=True)
-        await bot.send_message(PM, A.format(message.from_user.first_name, message.from_user.username, message.from_user.id, message.from_user.mention, message.from_user.id))
-        await message.reply_to_message.forward(chat_id=PM)
-        await k.edit_text("**Thanks for Reporting.** ❤️\n..\n**I have forwarded your message to my Owner. He will reply you When ever he will be free. ✌️**")
-    else:
-        await message.reply_text("**Please Send me a message** and **Then Reply that message with __/report__ , so that I can report that to my Owner**")
+        chat_id = message.chat.id
+        reporter = str(message.from_user.id)
+        mention = message.from_user.mention
+        success = True
+        report = f"𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})" + "\n"
+        report += f"𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {message.reply_to_message.link}"
+        async for admin in bot.get_chat_members(chat_id=message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+            if not admin.user.is_bot: 
+                try:
+                    reported_post = await message.reply_to_message.forward(admin.user.id)
+                    await reported_post.reply_text(
+                        text=report,
+                        chat_id=admin.user.id,
+                        disable_web_page_preview=True
+                    )
+                    success = True
+                except:
+                    pass
+            else: 
+                pass
+        if success:
+            await message.reply_text(script.REPORT_MSG)
