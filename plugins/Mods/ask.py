42d8d580ff1f4a0238e8b9144304f452b01dab7d
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-18 17:44:16 +0530

    Update ask.py

--- plugins/Mods/ask.py
+++ plugins/Mods/ask.py
@@ -1,74 +1,53 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import requests
 from info import LOG_CHANNEL, GOOGLE_API_KEY
 import google.generativeai as genai
 
 genai.configure(api_key=GOOGLE_API_KEY)
-# how to get the api key == https://t.me/sd_bots/256 (copy this link and search on telegram)
 
-@Client.on_message(filters.command("ask") & filters.group)
+@Client.on_message(filters.command("ask"))
 async def ai_generate(client, message):
-    user_input = message.text.split()[1:]
+   user_input = message.text.split()[1:]
 
-    if not user_input:
-        await message.reply_text("Please provide your question after /ai")
-        return
+   if not user_input:
+       await message.reply_text("/ask Where is TajMahal?")
+       return
 
-    user_input = " ".join(user_input)
-    s = await message.reply_sticker("CAACAgUAAxkBAAEBVDllqO69p9KDoRHJZC27Zr2Bes80MQACywYAArjkGVbUQEO9fppO2x4E")
-  
-    if user_input.lower() in ["who is your owner", "what is your owner name"]:  
-        buttons = [[
-            InlineKeyboardButton("developer", url="https://t.me/sd_bots")
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_sticker("CAACAgUAAxkBAAEBVDllqO69p9KDoRHJZC27Zr2Bes80MQACywYAArjkGVbUQEO9fppO2x4E")
-        await message.reply_text(text=f"ʜᴇʏ {message.from_user.mention}", reply_markup=reply_markup)
-        return
-      
-    generation_config = {
-        "temperature": 0.9,
-        "top_p": 1,
-        "top_k": 1,
-        "max_output_tokens": 2048,
-    }
+   user_input = " ".join(user_input)
 
-    safety_settings = [
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-    ]
+   generation_config = {
+       "temperature": 0.9,
+       "top_p": 1,
+       "top_k": 1,
+       "max_output_tokens": 2048,
+   }
 
-    model = genai.GenerativeModel(
-        model_name="gemini-pro",
-        generation_config=generation_config,
-        safety_settings=safety_settings
-    )
+   safety_settings = [
+       {
+           "category": "HARM_CATEGORY_HARASSMENT",
+           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+       },
+       {
+           "category": "HARM_CATEGORY_HATE_SPEECH",
+           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+       },
+       {
+           "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+       },
+       {
+           "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+       },
+   ]
 
-    prompt_parts = [user_input]
-    response = model.generate_content(prompt_parts)
-    await message.reply_text(response.text)
-    await client.send_message(LOG_CHANNEL, text=f"#google_ai ʀᴇǫᴜᴇsᴛ ғʀᴏᴍ {message.from_user.mention}\nǫᴜᴇʀʏ ɪs:- {user_input}")
-    await s.delete()
-@Client.on_message(filters.command("ai") & filters.private)
-async def ai_generate_private(client, message):
-  buttons = [[
-    InlineKeyboardButton("ɢʀᴏᴜᴘ", url="https://t.me/+DnmZbLjS0iw0YWI1")
-  ]]
-  reply_markup = InlineKeyboardMarkup(buttons)
-  await message.reply_sticker("CAACAgUAAxkBAAEBVDllqO69p9KDoRHJZC27Zr2Bes80MQACywYAArjkGVbUQEO9fppO2x4E")
-  await message.reply_text(text=f"ʜᴇʏ {message.from_user.mention}\nᴜsᴇ ᴛʜɪs ғᴇᴀᴛᴜʀᴇ ɪɴ ɢʀᴏᴜᴘ", reply_markup=reply_markup)
+   model = genai.GenerativeModel(
+       model_name="gemini-pro",
+       generation_config=generation_config,
+       safety_settings=safety_settings
+   )
+
+   prompt_parts = [user_input]
+   response = model.generate_content(prompt_parts)
+   await message.reply_text(response.text)
+   await client.send_message(LOG_CHANNEL, text=f"#google_ai ʀᴇǫᴜᴇsᴛ ғʀᴏᴍ {message.from_user.mention}\nǫᴜᴇʀʏ ɪs:- {user_input}")
