c87504c5884d54870154067f4c5726a393770000
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-20 20:03:11 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -2,13 +2,13 @@ from prettytable import PrettyTable
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
-from info import info
+from info import PREFIX
 from helper.utils import CMD_HELP
 from helper.basic import edit_or_reply
 from helper.utility import split_list
 from helper.utils import Automato
 
-@Client.on_message(filters.command(["help"], Config.PREFIX) & filters.private)
+@Client.on_message(filters.command(["help"], info.PREFIX) & filters.private)
 async def module_help(client: Client, message: Message):
     cmd = message.command
     help_arg = ""
