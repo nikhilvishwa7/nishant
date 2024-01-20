fab2ae0097bb5e0eb2ba318d89db8fe27b525984
ᴀsʜ <153292001+erotixe@users.noreply.github.com>
2024-01-20 19:32:13 +0530

    Create anime.py

---
+++ plugins/Extra/anime.py
@@ -0,0 +1,30 @@
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from info import info
+from helper.utils import Automato
+
+API_URL = "https://api.nekosapi.com/v2/images/random"
+
+
+@Automato.on_message(filters.command(["anime"], Config.PREFIX) & filters.private)
+async def random_anime(client: Client, message: Message):
+    # Send the "Processing..." message
+    dx = await message.reply_text("Fetching a random anime image...")
+
+    # Make the API request
+    try:
+        response = requests.get(API_URL)
+        response.raise_for_status()
+        data = response.json()["data"]["attributes"]
+        image_url = data["file"]
+        title = data["title"]
+    except (requests.exceptions.RequestException, KeyError):
+        await dx.edit("Failed to fetch a random anime image.")
+        return
+
+    # Send the image and title as a reply
+    await client.send_photo(message.chat.id, image_url, caption=f"**Title:** {title}")
+
+    # Edit the original message to indicate success
+    await dx.edit("Random anime image sent!")
